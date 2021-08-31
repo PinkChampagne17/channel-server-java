10826ab45949dd11d9eaae86805b577c394f870f
Pink Champagne <r2kou@outlook.com>
2021-08-31 18:44:40 +0800

    Config CORS

--- src/main/java/io/github/pinkchampagne17/channelserver/ChannelServerApplication.java
+++ src/main/java/io/github/pinkchampagne17/channelserver/ChannelServerApplication.java
@@ -2,12 +2,28 @@ package io.github.pinkchampagne17.channelserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChannelServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChannelServerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOriginPatterns("*")
+						.allowedMethods("OPTIONS", "HEAD", "GET", "POST", "PUT", "PATCH", "DELETE")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
